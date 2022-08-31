921f9c1536d4cfe3d23f2f410e21a056aad0cba1
Rodrigo Souza <0dricos@gmail.com>
2022-08-31 19:40:02 -0300

    e

--- src/main/java/com/example/apiforbruteforce/service/UsersService.java
+++ src/main/java/com/example/apiforbruteforce/service/UsersService.java
@@ -5,19 +5,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
-    Users userDataBase = new Users("admin", "012379", 0);
+    Users userDataBase = new Users("root", "012379", 0);
+    
     public boolean login(Users user) {
+        
         if(userDataBase.getQtdTentativasLoginIncorreto() <= 15) {
+            
             if(userDataBase.getUsername().equals(user.getUsername()) && userDataBase.getPassword().equals(user.getPassword())) {
-                System.out.println("Senha correta: " + user.getPassword());
                 userDataBase.setQtdTentativasLoginIncorreto(0);
                 return true;
             }
-            System.out.println("Senha incorreta!");
-            if(userDataBase.getQtdTentativasLoginIncorreto() == 15) {
-                System.out.println("Quantidade de tentativas de login inválidas ultrapassadas: " + userDataBase.getQtdTentativasLoginIncorreto());
-            }
-                userDataBase.setQtdTentativasLoginIncorreto(userDataBase.getQtdTentativasLoginIncorreto() + 1);
+            
+            userDataBase.setQtdTentativasLoginIncorreto(userDataBase.getQtdTentativasLoginIncorreto() + 1);
         }
 
         return false;
